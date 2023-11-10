8d3b8c92f2d3c682acccea2c5b7350ee07ac6417
nvnieuwk <nicolas.vannieuwkerke@ugent.be>
2023-11-10 10:24:05 +0100

    add simple if

--- convert/vcf.go
+++ convert/vcf.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -275,6 +276,63 @@ func resolveField(value string, values []string, header []string) string {
 			sum += float
 		}
 		return strconv.FormatFloat(sum, 'g', 5, 64)
+	case "if":
+		// ~if <value1> <operator> <value2> <value_if_true> <value_if_false>
+		// supported operators: > < >= <= ==
+		v1 := input[1]
+		operator := input[2]
+		v2 := input[3]
+		vTrue := input[4]
+		vFalse := input[5]
+
+		floatV1, err1 := strconv.ParseFloat(v1, 64)
+		floatV2, err2 := strconv.ParseFloat(v2, 64)
+
+		floatOperators := []string{"<", ">", "<=", ">="}
+		if slices.Contains(floatOperators, operator) && (err1 != nil || err2 != nil) {
+			log.Fatalf("Failed to parse the values (%v and %v) to a float: %v and %v", v1, v2, err1, err2)
+		}
+
+		switch operator {
+		case "<":
+			if floatV1 < floatV2 {
+				return vTrue
+			} else {
+				return vFalse
+			}
+		case ">":
+			if floatV1 > floatV2 {
+				return vTrue
+			} else {
+				return vFalse
+			}
+		case ">=":
+			if floatV1 >= floatV2 {
+				return vTrue
+			} else {
+				return vFalse
+			}
+		case "<=":
+			if floatV1 <= floatV2 {
+				return vTrue
+			} else {
+				return vFalse
+			}
+		case "==":
+			if err1 == nil && err2 == nil {
+				if floatV1 == floatV2 {
+					return vTrue
+				} else {
+					return vFalse
+				}
+			} else {
+				if v1 == v2 {
+					return vTrue
+				} else {
+					return vFalse
+				}
+			}
+		}
 	}
 
 	return ""
